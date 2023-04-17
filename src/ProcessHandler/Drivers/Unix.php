40873470a6437ea0a8ab69fc169b2a89a8b36b91
bonifacepereira <bonifacepereira@gmail.com>
2023-04-18 01:21:30 +0530

    update unix and windows drivers

--- src/ProcessHandler/Drivers/Unix.php
+++ src/ProcessHandler/Drivers/Unix.php
@@ -6,53 +6,63 @@ use Craftpip\ProcessHandler\Process as Process2;
 use Symfony\Component\Process\Process;
 
 class Unix implements DriversInterface {
-    /**
-     * @return \Craftpip\ProcessHandler\Process[]
-     */
-    function getAllProcesses () {
-        $process = new Process('ps -eo pid,time,size,user,session,cmd');
-        $process->run();
-        $op = trim($process->getOutput());
+      /**
+       * @return \Craftpip\ProcessHandler\Process[]
+       */
+      function getAllProcesses() {
+            $process = new Process([
+                'ps',
+                '-eo',
+                'pid,time,size,user,session,cmd',
+            ]);
+            $process->run();
+            $op = trim($process->getOutput());
 
-        return $this->parse($op);
-    }
+            return $this->parse($op);
+      }
 
-    /**
-     * @param $pid
-     *
-     * @return \Craftpip\ProcessHandler\Process[]
-     */
-    function getProcessByPid ($pid) {
-        $process = new Process("ps -p $pid -o pid,time,size,user,session,cmd");
-        $process->run();
-        $op = trim($process->getOutput());
+      /**
+       * @param $pid
+       *
+       * @return \Craftpip\ProcessHandler\Process[]
+       */
+      function getProcessByPid($pid) {
+            $process = new Process([
+                "ps",
+                "-p",
+                "$pid",
+                "-o",
+                "pid,time,size,user,session,cmd",
+            ]);
+            $process->run();
+            $op = trim($process->getOutput());
 
-        return $this->parse($op);
-    }
+            return $this->parse($op);
+      }
 
-    /**
-     * @param $output
-     *
-     * @return \Craftpip\ProcessHandler\Process[]
-     */
-    private function parse ($output) {
-        $op = explode("\n", $output);
+      /**
+       * @param $output
+       *
+       * @return \Craftpip\ProcessHandler\Process[]
+       */
+      private function parse($output) {
+            $op = explode("\n", $output);
 
-        $processes = array();
-        foreach ($op as $k => $item) {
-            if ($k < 1)
-                continue;
+            $processes = array();
+            foreach ($op as $k => $item) {
+                  if ($k < 1)
+                        continue;
 
-            $item = explode(" ", $item);
-            $line = array();
-            foreach ($item as $i) {
-                if ($i != '')
-                    $line[] = $i;
+                  $item = explode(" ", $item);
+                  $line = array();
+                  foreach ($item as $i) {
+                        if ($i != '')
+                              $line[] = $i;
+                  }
+                  $processName = implode(" ", array_slice($line, 5));
+                  $processes[] = new Process2($processName, $line[0], false, $line[4], $line[2] . ' KB', 'RUNNING', $line[3], $line[1], false);
             }
-            $processName = implode(" ", array_slice($line, 5));
-            $processes[] = new Process2($processName, $line[0], false, $line[4], $line[2] . ' KB', 'RUNNING', $line[3], $line[1], false);
-        }
 
-        return $processes;
-    }
+            return $processes;
+      }
 }
